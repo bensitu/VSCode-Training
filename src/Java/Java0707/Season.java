2f2ffbd9fa22261d30d61d393045dbe75eeec75d
BenSiTu <sito163@163.com>
2022-07-12 21:00:05 +0900

    Updated date

--- src/Java/Java0707/Season.java
+++ src/Java/Java0707/Season.java
@@ -9,37 +9,36 @@ public class Season {
         do {
             System.out.println("Please enter your number:");
             val = input.next();
-            System.out.println("You have enter the number: "+val);
-            switch(val){
-                case "1":
-                case "2":
+            System.out.println("You have enter the number: " + val);
+            switch (val) {
                 case "3":
-                System.out.println("This is Spring!");
-                break;
-
                 case "4":
                 case "5":
-                case "6":
-                System.out.println("This is Summer!");
-                break;
+                    System.out.println("This is Spring!");
+                    break;
 
+                case "6":
                 case "7":
                 case "8":
-                case "9":
-                System.out.println("This is Autumn!");
-                break;
+                    System.out.println("This is Summer!");
+                    break;
 
+                case "9":
                 case "10":
                 case "11":
+                    System.out.println("This is Autumn!");
+                    break;
+
                 case "12":
-                System.out.println("This is Winter!");
-                break;
+                case "1":
+                case "2":
+                    System.out.println("This is Winter!");
+                    break;
 
                 default:
-                System.out.println("Please input the number between 1-12");
-                break;
+                    System.out.println("Please input the number between 1-12");
+                    break;
             }
-
 
         } while (!"#".equals(val));
         System.out.println("Sysem shutdown.");
