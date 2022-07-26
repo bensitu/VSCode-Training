a0d530cc4e739c45beb705b3bd9c91d5a7d48a50
bensitu <sito163@163.com>
2022-07-26 16:33:02 +0900

    Exercise 0726

--- src/Java/Java0726/Question6/Main.java
+++ src/Java/Java0726/Question6/Main.java
@@ -3,23 +3,24 @@ package Java.Java0726.Question6;
 public class Main {
     public static void main(String[] args) {
         try {
-            // double weight = Double.parseDouble(args[0]);
-            // double height = Double.parseDouble(args[1]);
+            double weight = Double.parseDouble(args[0]);
+            double height = Double.parseDouble(args[1]);
             // double weight = -50;
             // double height = -100;
-            double weight = 65;
-            double height = 1.7;
+            // double weight = 65;
+            // double height = 1.7;
             System.out.println("BMI = " + getBMI(weight, height));
         } catch (IllegalArgumentException e) {
-            e.getMessage();
+            System.out.println(e.getMessage());
+
         } catch (Exception e) {
             System.out.println("２つの数値を指定してください");
         }
     }
 
-    public static double getBMI(double weight, double height) throws Exception {
+    public static double getBMI(double weight, double height) throws IllegalArgumentException {
         if (weight < 0 || height < 0) {
-            throw new Exception("負数を用いることはできません");
+            throw new IllegalArgumentException("負数を用いることはできません");
         } else {
             return weight / (height * height);
         }
